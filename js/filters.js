74daa265bf0706c79054e063d477e0d27898af62
brunoscopelliti <bru.scopelliti@gmail.com>
2013-05-01 16:17:43 +0200

    fix
    copy/paste typo

--- js/filters.js
+++ js/filters.js
@@ -2,7 +2,7 @@
 
 var filters = angular.module('myApp.filters', []);
 
-filters.filter('i18n', ['$rootScope', 'currentLocale', function ($rootScope, locale) {
+filters.filter('i18n', ['currentLocale', function (locale) {
 	return function (key, p) {
 		if (typeof locale[key] != 'undefined' && locale[key] != '') {
 			return (typeof p === "undefined") ? locale[key] : locale[key].replace('@{}@', p);
